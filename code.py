cc68dd3adfacf50ea69c273ae226cb157e0cdd18
nuclearsheep <nuclearsheep53@Gmail.com>
2024-06-27 02:38:17 -0700

    Added toggle keys
    Shift, Control, and Alt are toggled, enabling shortcuts and capitals.

--- code.py
+++ code.py
@@ -1,12 +1,10 @@
 import board
 import time
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 import usb_hid
 time.sleep(1)
 keyboard = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(keyboard)
 
 import digitalio
 A1 = digitalio.DigitalInOut(board.A1)  # Input object
@@ -22,97 +20,111 @@ silence = 0
 
 UNIT_TIME = 120  # Time in mill's for a 'dit'
 letters = {
-    # Letters
-    ".-": "A",
-    "-...": "B",
-    "-.-.": "C",
-    "-..": "D",
-    ".": "E",
-    "..-.": "F",
-    "--.": "G",
-    "....": "H",
-    "..": "I",
-    ".---": "J",
-    "-.-": "K",
-    ".-..": "L",
-    "--": "M",
-    "-.": "N",
-    "---": "O",
-    ".--.": "P",
-    "--.-": "Q",
-    ".-.": "R",
-    "...": "S",
-    "-": "T",
-    "..-": "U",
-    "...-": "V",
-    ".--": "W",
-    "-..-": "X",
-    "-.--": "Y",
-    "--..": "Z",
-    # Numbers
-    ".----": "1",
-    "..---": "2",
-    "...--": "3",
-    "....-": "4",
-    ".....": "5",
-    "-....": "6",
-    "--...": "7",
-    "---..": "8",
-    "----.": "9",
-    "-----": "0",
-    # Math
-    "-...-": "=",
-    "-.--.": "+",
-    "-.---": "-",
-    ".-.--": "*",
-    ".-..-": "/",
-    "--.--": "\\",
-    "---.-": "|",
-    ".--..": "(",
-    ".--.-": ")",
-    "--.-.": "{",
-    "--..-": "}",
-    "-..-.": "[",
-    "-..--": "]",
+	# Letters
+	".-": Keycode.A,
+	"-...": Keycode.B,
+	"-.-.": Keycode.C,
+	"-..": Keycode.D,
+	".": Keycode.E,
+	"..-.": Keycode.F,
+	"--.": Keycode.G,
+	"....": Keycode.H,
+	"..": Keycode.I,
+	".---": Keycode.J,
+	"-.-": Keycode.K,
+	".-..": Keycode.L,
+	"--": Keycode.M,
+	"-.": Keycode.N,
+	"---": Keycode.O,
+	".--.": Keycode.P,
+	"--.-": Keycode.Q,
+	".-.": Keycode.R,
+	"...": Keycode.S,
+	"-": Keycode.T,
+	"..-": Keycode.U,
+	"...-": Keycode.V,
+	".--": Keycode.W,
+	"-..-": Keycode.X,
+	"-.--": Keycode.Y,
+	"--..": Keycode.Z,
+	# Numbers
+	".----": Keycode.ONE,
+	"..---": Keycode.TWO,
+	"...--": Keycode.THREE,
+	"....-": Keycode.FOUR,
+	".....": Keycode.FIVE,
+	"-....": Keycode.SIX,
+	"--...": Keycode.SEVEN,
+	"---..": Keycode.EIGHT,
+	"----.": Keycode.NINE,
+	"-----": Keycode.ZERO,
+	# Math
+	"-...-": Keycode.EQUALS,
+	"-.--.": Keycode.KEYPAD_PLUS,
+	"-.---": Keycode.MINUS, #-_
+	".-.--": Keycode.KEYPAD_ASTERISK,
+	".-..-": Keycode.FORWARD_SLASH,  # /&?
+	"--.--": Keycode.BACKSLASH,  # & |
+	".--..": 116,  # (
+	".--.-": 117,  # )
+	"--.-.": 118,  # {
+	"--..-": 119,  # }
+	"-..-.": "[",
+	"-..--": "]",
 	# Punctuation
-	".-.-.-": ".",
-	"--..--": ",",
-	"---...": ":",
-	"..-.-": ";",
+	".-.-.-": Keycode.PERIOD,
+	"--..--": Keycode.COMMA,  # ,<
+	"---...": "Keycode.COLON",
+	"..-.-": Keycode.SEMICOLON,  #;:
 	"..--..": "?",
 	"..--.": "!",
-	".-..-.": '"',
-	".----.": "'",
+	".-..-.": Keycode.QUOTE,
+	".----.": "'",  # May be tied to quote
+	"---.-": Keycode.POUND,  # #&~
+	
 	# Arrows
-	"..-..": "UP",
-	".-.-.": "DOWN",
-	".-...": "LEFT",
-	"...-.": "RIGHT",
+	"..-..": Keycode.UP_ARROW,
+	".-.-.": Keycode.DOWN_ARROW,
+	".-...": Keycode.LEFT_ARROW,
+	"...-.": Keycode.RIGHT_ARROW,
 	# Functions
-	"-.....": "COPY",
-	".-....": "CUT",
-	"..-...": "PASTE",
-	"...-..": "UNDO",
-	".....-": "SAVE",
 	".-.-": Keycode.ESCAPE,
-	"---.": " ",
-	"----": "\b",  # BACK
+	"---.": Keycode.SPACE,
+	"----": Keycode.BACKSPACE,  # BACK
 	".---.": Keycode.ENTER,
 	"....--": Keycode.DELETE,
+	".----.": 43,  # TAB
+	# Toggles
 	"..--": Keycode.SHIFT,
 	"-.-..": Keycode.CONTROL,
 	"-.-.-": Keycode.ALT,
+	# Unused
+	"-.....": 124,  # COPY
+	".-....": 123,  # CUT
+	"..-...": 125,  # PASTE
+	"...-..": 122,  # UNDO
+	".....-": "SAVE",  # SAVE
+	"..--..": "?",
+	"..--.": "!",
+	"---...": "Keycode.COLON",
 	}
 
+toglist = {"..--": 0, "-.-..": 0, "-.-.-": 0,}  # Specific keys to be toggled
 
-def emitLetter(string):
+def emitLetter(code):
     try:
-        keyboard_layout.write(letters[string])
-# keyboard_layout.write may be redundant if we'll use keycodes...
-# keyboard.press(Keycode.<key>) may be preferred assuming we can
-# install it into a dictionary. But remember you must release too.
-    except:
-        pass
+        keyboard.press(letters[code])
+        time.sleep(0.001)
+        try: 
+            toglist[code]  # Check wanted list
+        except:
+            keyboard.release(letters[code])
+            print("normal character")
+        else:  # Match Found
+            if toglist[code]:
+                keyboard.release(letters[code])
+                print("released")
+            toglist[code] ^= 1
 
 while True:
     # Start/stop timers depending on up or down
